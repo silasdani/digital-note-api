80df4b4b5f34219c3fe130004ec3697081bbacdb
silasdani <danielsilas23@yahoo.com>
2022-05-23 16:23:38 +0300

    seed with data

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -20,7 +20,7 @@
 #
 class Exam < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :questions
+  has_many :questions, dependent: :destroy
   validates :access_key, uniqueness: { on: :create, message: 'Access key must be unique' }
 
   enum status: { active: 0, draft: 1, archived: 2, completed: 3 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,41 @@
+require 'faker'
+
 Setting.create_or_find_by!(key: 'min_version', value: '0.0')
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') 
+
+  User.create!(
+    email: "daniel@yahoo.com",
+    first_name: "Silas",
+    last_name: "Daniel",
+    address: "RO",
+    username: "miau",
+    phone_no: "0711133311",
+    password: "password",
+    password_confirmation: "password"
+  )
+  
+  (1..3).each do
+    questions = []
+
+    (1..3).each do |no|
+      questions << Question.new(
+        no: no,
+        text_statement: Faker::Lorem.question(word_count: 4, supplemental: true),
+        question_type: "text"
+      )
+    end
+
+    exam = Exam.new(
+      user_id: User.first.id,
+      name: "Love story"
+    )
+
+    exam.active!
+    exam.low_security!
+    exam.questions = questions
+
+    exam.save!
+
+  end
+end
