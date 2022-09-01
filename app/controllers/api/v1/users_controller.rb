c4e4933617ea2568fba6970bbbb7f334368246a5
silasdani <danielsilas23@yahoo.com>
2022-09-01 16:06:51 +0300

    awesome

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class UsersController < Api::V1::ApiController
-      before_action :auth_user
+      # metoda :auth_user se ruleză înainte de orice request, autorizând
+      # astfel orice utilizator
+      before_action :auth_user 
 
       def show; end
 
@@ -18,8 +20,10 @@ module Api
 
       def user_params
         convert_file_params [:profile_pic],
-                            params.require(:user).permit(:username, :first_name, :last_name, :email, :profile_pic,
-                                                         :phone_no, :address)
+        params.require(:user).permit(
+          :username, :first_name, :last_name,
+          :email, :profile_pic, :phone_no, :address
+        )
       end
     end
   end
